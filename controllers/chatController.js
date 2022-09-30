0e97ce1e2fe2c09049f4953224bf853f81a4f9b6
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2022-09-30 18:48:15 +0530

    rename group chat

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -101,5 +101,27 @@ const createGroupChat = asyncHandler(async(req,res)=>{
     }
 });
 
+const renameGroup = asyncHandler(async(req,res)=>{
+     const { chatId, chatName } = req.body;
 
-module.exports= { accessChat, fetchChats, createGroupChat };
+     const updatedChat = await Chat.findByIdAndUpdate(
+        chatId, // find the chat by id
+        {
+            chatName: chatName  // update the chat name
+        },
+        {
+            new: true // return the new chat name
+        },
+     ).populate("users", "-password")
+     .populate("groupAdmin", "-password");
+
+     if(!updatedChat){
+        res.status(404);
+        throw new Error("Chat Not Found");
+     }else{
+        res.json(updatedChat);
+     }
+});
+
+
+module.exports= { accessChat, fetchChats, createGroupChat, renameGroup};
