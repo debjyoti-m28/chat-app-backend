8dff7419c21269b025725d818e7ca5748f874cc2
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2022-10-23 12:20:41 +0530

    added send message api

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRouts = require("./routes/chatRouts");
+const messageRoutes = require("./routes/messageRoutes")
 
 const app = express();
 dotenv.config();
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRouts);
+app.use("/api/message",messageRoutes);
 
 app.listen(5001, ()=>{
       console.log("Backend server is running...");
